2fdf502deab61b0540fd38e4ef3f39a61a0ce73a
farhan-dqz-Julie <k854747@gmail.com>
2021-08-17 14:50:40 +0530

    deleted find command

--- bot.js
+++ bot.js
@@ -157,7 +157,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                 let pp
                 try { pp = await conn.getProfilePicture(msg.messageStubParameters[0]); } catch { pp = await conn.getProfilePicture(); }
                 await axios.get(pp, {responseType: 'arraybuffer'}).then(async (res) => {
-                await conn.sendMessage(msg.key.remoteJid, conn.user.jid,  res.data, MessageType.image, {caption:  'Hlo' + conn.username + gb.message }); });
+                await conn.sendMessage(msg.key.remoteJid, res.data, MessageType.image, {caption:  gb.message }); });
             }
             return;
         } else if (msg.messageStubType === 27 || msg.messageStubType === 31) {
@@ -167,7 +167,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                let pp
                 try { pp = await conn.getProfilePicture(msg.messageStubParameters[0]); } catch { pp = await conn.getProfilePicture(); }
                 await axios.get(pp, {responseType: 'arraybuffer'}).then(async (res) => {
-                await conn.sendMessage(msg.key.remoteJid, conn.user.jid,  res.data, MessageType.image, {caption:  '*Hlo' + conn.username + '*\n' +  gb.message }); });
+                await conn.sendMessage(msg.key.remoteJid, res.data, MessageType.image, {caption:  gb.message }); });
             }
             return;
         }
