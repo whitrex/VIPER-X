8848c586959ca69426981aa8c110398221b0d5ce
farhan-dqz-Julie <k854747@gmail.com>
2021-08-17 10:07:55 +0530

    first commit

---
+++ plugins/del.js
@@ -0,0 +1,17 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const DEL_DESC = "Deletes The Replied Message Send By The Bot."
+
+let farhan = conf.WORKTYPE == 'public' ? false : true
+
+Asena.addCommand({pattern: 'del', fromMe: farhan, desc: DEL_DESC}, (async (message, match) => {
+
+  await message.reply_message.delete();
+
+}));
+
+Asena.addCommand({pattern: 'delete', fromMe: farhan, desc: DEL_DESC}, (async (message, match) => {
+
+  await message.reply_message.delete();
+
+}));
